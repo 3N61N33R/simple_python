ffef0ec5559f8a3d6afaa5efcca1d2273efe56da
Grace Zawadi <g.zawadi.w@gmail.com>
2021-04-01 09:48:14 +0300

    fractional part

--- code/conditionals.py
+++ code/conditionals.py
@@ -82,3 +82,24 @@ def sum(x, y):
 
 
 print(sum(sum(1, 2), sum(3, 4)))
+
+
+def fractional_part(numerator, denominator):
+    # Operate with numerator and denominator to
+    # keep just the fractional part of the quotient
+    if numerator > 0 and denominator > 0:
+        result = (numerator % denominator) / denominator
+        if result > 0:
+            return result
+        else:
+            return 0
+    else:
+        return 0
+
+
+print(fractional_part(5, 5))  # Should be 0
+print(fractional_part(5, 4))  # Should be 0.25
+print(fractional_part(5, 3))  # Should be 0.66...
+print(fractional_part(5, 2))  # Should be 0.5
+print(fractional_part(5, 0))  # Should be 0
+print(fractional_part(0, 5))  # Should be 0

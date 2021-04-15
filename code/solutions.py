9ee01ae2f6e6b1b810122869460558f887b7b4a1
Grace Zawadi <g.zawadi.w@gmail.com>
2021-04-15 21:00:17 +0300

    proper divisors

--- code/solutions.py
+++ code/solutions.py
@@ -74,4 +74,38 @@ def divisorSum( n ):
       
 # Driver code
 print( divisorSum(3))
-print( divisorSum(36))
+print(divisorSum(36))
+
+# PYTHON program to find sum of all
+# divisors of a natural number
+import math
+     
+# Function to calculate sum of all proper
+# divisors num --> given natural number
+def divSum(num) :
+     
+    # Final result of summation of divisors
+    result = 0
+     
+    # find all divisors which divides 'num'
+    i = 2
+    print("<><><><>",num ** 0.5)
+
+    while i <= (math.sqrt(num)) :
+       
+        # if 'i' is divisor of 'num'
+        if (num % i == 0) :
+       
+            # if both divisors are same then
+            # add it only once else add both
+            if (i == (num / i)) :
+                result = result + i;
+            else :
+                result = result +  (i + num/i);
+        i = i + 1
+         
+    # Add 1 to the result as 1 is also
+    # a divisor
+    print(result + 1)
+    return result + 1
+    
